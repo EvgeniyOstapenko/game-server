0f0043511f7aad6c5c1ce80399eea2c909ded124
Evgeniy <evgeniy.ostapenko.1991@mail.ru>
2020-12-07 17:16:52 +0300

    property changed

--- src/test/java/client/connection/ClientConnection.java
+++ src/test/java/client/connection/ClientConnection.java
@@ -50,7 +50,13 @@ public class ClientConnection {
                             pipeline.addLast("messageHandler", messageHandler);
                         }
                     });
-            channel = bootstrap.connect(new InetSocketAddress(host, port)).syncUninterruptibly().channel();
+//            channel = bootstrap.connect(new InetSocketAddress(host, port)).syncUninterruptibly().channel();
+            ChannelFuture connect = bootstrap.connect(new InetSocketAddress(host, port));
+            ChannelFuture channelFuture = connect.syncUninterruptibly();
+            channel = channelFuture.channel();
+
+            System.out.println(1111);
+
         } catch (Exception e) {
             log.error(e.toString(), e);
             group.shutdownGracefully().syncUninterruptibly();
